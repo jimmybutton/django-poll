4198588c5d329f9ba4ad9f37e592fb85a638802d
jimmybutton <samu.liedtke@gmail.com>
2020-08-19 18:46:36 +0100

    styling the app and add a chart

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,7 +11,18 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+    def num_choices(self):
+        return self.choice_set.count()
+    
+    def num_votes(self):
+        return sum(c.votes for c in self.choice_set.all())
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,27 +3,46 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
+    # context_object_name = "latest_question_list"
+    paginate_by = 3
 
     def get_queryset(self):
         """Return the last five published quesions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")  # [:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        question = get_object_or_404(Question, pk=self.kwargs['pk'])
+        choices = question.choice_set.all()
+        context['chartdata'] = {
+            'data': [c.votes for c in choices], 
+            'labels': [c.choice_text for c in choices],
+            }
+        return context
 
 
 def vote(request, question_id: int) -> HttpResponse:
